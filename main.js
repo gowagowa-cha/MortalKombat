30d9d670e51bd39638076f81a4be982ee4121707
Gashun Chagdonov <gowagowa1990@gmail.com>
2021-06-08 14:55:21 +0300

    Разделил changeHP() на три отделные функции, добавил создание кнопки рестарт и сделал для нее reload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const $arenas = document.querySelector('.arenas');
-const $randomButton = document.querySelector('.button')
+const $randomButton = document.querySelector('.button');
 
 const player1 = {
 	player: 1,
@@ -9,7 +9,10 @@ const player1 = {
 	weapon: ['knife'],
 	attack: function(name){
 		console.log(name + 'fight...');
-	}
+	},
+	changeHP: changeHP,
+	elHP: elHP,
+	renderHP: renderHP
 }
 const player2 = {
 	player: 2,
@@ -19,7 +22,10 @@ const player2 = {
 	weapon: ['knife'],
 	attack: function(name){
 		console.log(name + 'fight...');
-	}
+	},
+	changeHP: changeHP,
+	elHP: elHP,
+	renderHP: renderHP
 }
 
 
@@ -52,42 +58,76 @@ function createPlayer(play){
 	return $player
 }
 
-function randomHP(min, max) {
-	const randomNum = min + Math.random() * (max + 1 - min);
-	console.log(Math.round(randomNum));
-	return Math.round(randomNum)
-	
+
+function getRandom(num) {
+	return Math.ceil(Math.random() * num)
 }
 
-function changeHP(player) {
-	const $playerLife = document.querySelector('.player'+ player.player +' .life');
-	player.hp -= randomHP(1, 20);
-	$playerLife.style.width = player.hp + '%'
-	console.log('####', player.hp);
+// Функция отображения здоровья
+function renderHP() {
+	this.elHP().style.width = this.hp + '%';
+}
 
-	if(player.hp <= 0) {
-		player.hp = 0;
-		$playerLife.style.width = `${0}%`
-		$arenas.appendChild(playerLose(player.name))
-		$randomButton.style.display = 'none'
+// Функция выбора персонажа
+function elHP() {
+	return document.querySelector('.player'+ this.player +' .life');
+}
+// Функция изменения здоровья
+function changeHP(num) {
+	this.hp -= num;
+	if(this.hp <= 0) {
+		this.hp = 0;
 	}
+	 return this.hp
 }
+
+
+function createReloadButton() {
+	const $reloadWrap = createElement('div', 'reloadWrap')
+	const $btn = createElement('button', 'button')
+	$btn.innerText = 'Restart';
+	$btn.addEventListener('click', () => {
+		window.location.reload()
+	})
+	$reloadWrap.appendChild($btn)
+	$arenas.appendChild($reloadWrap);
+	return $reloadWrap
+}
+
+
+
 
 $randomButton.addEventListener('click', function() {
-	changeHP(player1)
-	changeHP(player2)
+	player1.changeHP(getRandom(20))
+	player2.changeHP(getRandom(20))
+	player1.renderHP(elHP())
+	player2.renderHP(elHP())
+
+	
+	if(player1.hp === 0 || player2.hp === 0) {
+		$randomButton.disabled = true;
+		$arenas.appendChild(createReloadButton())	
+	}
+
+
+	if(player1.hp === 0 && player1.hp < player2.hp) {
+		$arenas.appendChild(playerWins(player2.name))
+	} else if (player2.hp === 0 && player2.hp < player1.hp) {
+		$arenas.appendChild(playerWins(player1.name))
+	} else if (player1.hp === 0 && player2.hp === 0) {
+		$arenas.appendChild(playerWins())
+	}
 
 })
 
-const playerLose = (name) => {
-	const $loseTitle = createElement('div', 'loseTitle')
-	$loseTitle.innerText = name + ' Lose';
-	return $loseTitle
-}
-const playerWin = (name) => {
-	const $loseTitle = createElement('div', 'loseTitle')
-	$loseTitle.innerText = name + ' win';
-	return $loseTitle
+const playerWins = (name) => {
+	const $loseWin = createElement('div', 'loseTitle')
+	if (name) {
+		$loseWin.innerText = name + ' win';
+	} else {
+		$loseWin.innerText = 'Draw'
+	}
+	return $loseWin
 }
 
 $arenas.appendChild(createPlayer(player1));
